5762fbf9199eff886f71d4e2b1fa18d1d68950a5
anjana119 <anjanasamarasinghe123@gmail.com>
2024-01-30 17:49:05 +0530

    modify folder structure

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(Options=>Options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
